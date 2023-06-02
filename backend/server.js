7222d1516f1d546e4299158626b36ad22fd2ae3f
guy-saloniki <guysalonik@walla.com>
2023-06-02 12:44:11 +0300

    Bug fix

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import uploadRoutes from './routes/uploadRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -21,10 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 
 //Cookie parser middleware
 app.use(cookieParser());
-
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
